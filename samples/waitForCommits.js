8c8c83f59f0a88f970ec1c17723a1c4b0cfcb874
GiacomoAgostini <giacomo.agostini@deltatre.com>
2016-11-17 16:59:04 +0100

    polling commit stream wip

--- samples/waitForCommits.js
+++ samples/waitForCommits.js
@@ -2,6 +2,21 @@
 
 const EventStore = require("../src/EventStore");
 const ProgressLogger = require("./progress-logger").ProgressLogger;
+// const Readable = require("stream").Readable;
+//
+// class PollingCommitStream extends Readable {
+// 	constructor(opt) {
+// 		super(opt);
+// 		this.bucket = opt.bucket;
+// 		this.startsFromRevision = opt.lastRevision || 1;
+// 	}
+//
+// 	_read(){
+// 		var stream=this.bucket.getCommitsStream({ fromBucketRevision: this.startsFromRevision});
+// 		this.push(stream);
+// 	}
+// }
+
 
 // set SAMPLE_URL=mongodb://localhost:27017/Forge
 const sampleUrl = process.env.SAMPLE_URL;
@@ -12,6 +27,7 @@ const sampleUrl = process.env.SAMPLE_URL;
 
 let progress = new ProgressLogger();
 let eventStore = new EventStore({url: sampleUrl});
+
 eventStore.connect()
 .then(() => {
 	let bucket = eventStore.bucket("wcm");
